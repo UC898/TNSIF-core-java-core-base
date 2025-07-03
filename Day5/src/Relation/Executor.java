3dfdcdc234000601266c261369263aa3db5cc450
chaud <chaud@Shivani>
2025-07-03 12:32:53 +0530

    Day5

---
+++ Day5/src/Relation/Executor.java
@@ -0,0 +1,15 @@
+package Relation;
+
+public class Executor {
+	public static void main(String[] args) {
+		 {
+		address obj1=new address("chikhali","pune",411062);
+		
+		person obj2=new person("Umesh",8983);
+		obj2.display();
+		
+		  		  
+	}
+	
+}
+}
