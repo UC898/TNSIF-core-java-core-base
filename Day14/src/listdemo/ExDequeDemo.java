349b321600b8a506c491b01f5e00593de20faf46
chaud <chaud@Shivani>
2025-07-14 23:44:17 +0530

    Day14

---
+++ Day14/src/listdemo/ExDequeDemo.java
@@ -0,0 +1,34 @@
+package listdemo;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ExDequeDemo {
+
+	public static void main(String[] args) {
+	
+		Deque<String> deque1=new ArrayDeque<String>();
+		deque1.add("raj");
+		deque1.add("anand");
+		deque1.add("Shreya");
+		deque1.add("hitesh");
+		
+		System.out.println(deque1);
+		
+		deque1.pollLast();
+		System.out.println(deque1);
+		
+		deque1.pollFirst();
+		System.out.println(deque1);
+		
+		for(String str : deque1)
+		{
+			System.out.println("queue is :"+ str);
+		}
+		
+		System.out.println(deque1);
+		
+
+	}
+
+}
